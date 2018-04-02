ceddc54ce1b34a7250ab56173b45e1d045e69de3
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-02 20:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
   delete newObject.key
-  return object
+  return newObject
 }
-
-function 
