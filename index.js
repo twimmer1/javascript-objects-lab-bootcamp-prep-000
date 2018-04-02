23169644cdc6daadc7b6b6b37f56e04d5e5a0eff
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-02 21:03:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
   return newObject
 }
 
-function destructiveDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
   }
